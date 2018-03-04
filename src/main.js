5b7c5572ef575f7f2645181fa869da76b8138da6
Kevin Oliveira <me@kevinoliveira.com.br>
2018-03-04 00:54:53 -0300

    Fix element without sibling

--- src/main.js
+++ src/main.js
@@ -19,10 +19,16 @@
 
     function disableSiblings() {
       $input.eventListener('on', 'change', function() {
-        if (this.selectedIndex != 0)
+        if (this.nextElementSibling === null)
+          return;
+        if (this.selectedIndex !== 0)
           return this.nextElementSibling.removeAttribute('disabled');
         return this.nextElementSibling.setAttribute('disabled', 'disabled');
       });
+    }
+
+    function hiddenContent() {
+
     }
   }
 
