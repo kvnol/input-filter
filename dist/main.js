94ec4dae253640cfe9eb776acfce7ba64eaafa22
Kevin Oliveira <me@kevinoliveira.com.br>
2018-03-04 00:26:25 -0300

    Create disableSiblings function

---
+++ dist/main.js
@@ -0,0 +1,30 @@
+(function(DOM) {
+  'use strict';
+
+  function filter(element, hasSiblings) {
+    var $input = new DOM(element);
+    $input.eventListener('on', 'change', onChangeValue);
+
+    function onChangeValue() {
+      if (hasSiblings === true) {
+        $input.forEach(function(item) {
+          var value = item.options;
+        });
+        return disableSiblings();
+      }
+
+      var value = this.options;
+      return value;
+    }
+
+    function disableSiblings() {
+      $input.eventListener('on', 'change', function() {
+        if (this.selectedIndex != 0)
+          return this.nextElementSibling.removeAttribute('disabled');
+        return this.nextElementSibling.setAttribute('disabled', 'disabled');
+      });
+    }
+  }
+
+  window.filter = filter;
+})(window.DOM);
